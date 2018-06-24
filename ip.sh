7f42b9c73d3d91cc5e338540fe6a5e09b56c669d
Super-box <37006424+Super-box@users.noreply.github.com>
2018-06-23 19:35:51 -0500

    Update ip.sh

--- ip.sh
+++ ip.sh
@@ -5,4 +5,4 @@ rm -rf /var/lib/dhclient/*
 dhclient -v eth0                                                                
 #sleep 3                                                                        
 ip=$(curl -s whatismyip.akamai.com)                                             
-curl "https://api.telegram.org/bot603541irquijWZw0/sendMessage?text=New ip: \`\`\`$ip\`\`\`&chat_id=-10047076&parse_mode=Markdown"
+echo $ip
