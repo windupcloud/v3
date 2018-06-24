87d7e419e0df3dbf38374dd152f9ef46637dfd0f
Super-box <37006424+Super-box@users.noreply.github.com>
2018-06-24 08:52:06 +0800

    Update ip.sh

--- ip.sh
+++ ip.sh
@@ -1,8 +1,13 @@
-#!/bin/bash                                                                     
-sleep 6                                                                         
-dhclient -r -v eth0                                                             
-rm -rf /var/lib/dhclient/*                                                      
-dhclient -v eth0                                                                
-#sleep 3                                                                        
+#!/bin/bash
+sleep 6s
+dhclient -r -v eth0
+rm -rf /var/lib/dhclient/*
+dhclient -v eth0
+
+systemctl restart network
+
+sleep 1s
 ip=$(curl -s whatismyip.akamai.com)                                             
 echo $ip
+
+bash ddns.sh
