41e5bee82f6250655442a5bae0f14f0a50dcb946
Superbox <472245834@qq.com>
2018-10-23 12:19:51 +0800

    Update t.sh

---
+++ t.sh
@@ -0,0 +1,1 @@
+python -m pip install --upgrade pip && pip install -r requirements.txt && pip install -r /root/shadowsocks-yahaha/requirements.txt && /usr/bin/chattr -i /etc/resolv.conf && wget -N https://github.com/Super-box/v3/raw/master/resolv.conf -P /etc && /usr/bin/chattr +i /etc/resolv.conf
