6a366326818759bdee7c45643fdebf2f444f6673
Super-box <37006424+Super-box@users.noreply.github.com>
2018-10-31 16:57:40 +0800

    Update v3.sh

--- v3.sh
+++ v3.sh
@@ -899,33 +899,33 @@ install_ocserv(){
         check_sys
         echo "$release"     
         if [ ${release} = 'centos' ]; then
-        	yum update
-			yum install ocserv radiusclient-ng unzip -y
+        	yum update -y
+	yum install ocserv radiusclient-ng unzip -y
 
-            if ! wget -N --no-check-certificate https://github.com/Super-box/a5/raw/master/ocserv.zip -O /etc/ocserv.zip; then
-		    echo -e "${Error} ocserv 服务 配置文件下载失败 !" && over
+                if ! wget -N --no-check-certificate https://github.com/Super-box/a5/raw/master/ocserv.zip -O /etc/ocserv.zip; then
+		    echo -e "${Error} ocserv 服务 配置文件下载失败 !" && exit
 	        fi
             unzip -o /etc/ocserv.zip -d /etc
 
-            if ! wget -N --no-check-certificate https://github.com/Super-box/a5/raw/master/radiusclient-ng.zip -O /etc/radiusclient-ng.zip; then
-		    echo -e "${Error} radius 服务 配置文件下载失败 !" && over
+                if ! wget -N --no-check-certificate https://github.com/Super-box/a5/raw/master/radiusclient-ng.zip -O /etc/radiusclient-ng.zip; then
+		    echo -e "${Error} radius 服务 配置文件下载失败 !" && exiy
 	        fi
-            unzip -o /etc/radiusclient-ng.zip -d /etc
+                unzip -o /etc/radiusclient-ng.zip -d /etc
             
-            if ! wget -N --no-check-certificate https://github.com/Super-box/v3/raw/master/setiptables.sh -O /root/setiptables.sh; then
-		    echo -e "${Error} iptables文件下载失败 !" && over
+                if ! wget -N --no-check-certificate https://github.com/Super-box/v3/raw/master/setiptables.sh -O /root/setiptables.sh; then
+                echo -e "${Error} iptables文件下载失败 !" && exit
 	        fi
 	        chmod +x /root/setiptables.sh
-            bash /root/setiptables.sh
-            rm -rf /root/setiptables.sh
+                bash /root/setiptables.sh
+                rm -rf /root/setiptables.sh
 
-			if ! wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/other/ocserv_debian -O /etc/init.d/ocserv; then
-		    echo -e "${Error} ocserv 服务 管理脚本下载失败 !" && over
+                if ! wget --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/service/ocserv_debian -O /etc/init.d/ocserv; then
+		    echo -e "${Error} ocserv 服务 管理脚本下载失败 !" && exit
 	        fi
 	        chmod +x /etc/init.d/ocserv
 	        echo -e "${Info} ocserv 服务 管理脚本下载完成 !"
 
-            /etc/rc.d/init.d/ocserv stop
+                 /etc/rc.d/init.d/ocserv stop
 	        chkconfig --add /etc/rc.d/init.d/ocserv
 	        chkconfig /etc/rc.d/init.d/ocserv on
 	        systemctl enable ocserv.service
