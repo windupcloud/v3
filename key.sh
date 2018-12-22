9dd0cd9e0449889f74a7fcd34d6dc4fd77f31809
Super-box <37006424+Super-box@users.noreply.github.com>
2018-12-22 16:20:35 +0800

    Create key.sh

---
+++ key.sh
@@ -0,0 +1,35 @@
+#/bin/sh
+apt-get update -y
+apt-get install curl -y
+yum clean all
+yum make cache
+yum install curl -y
+echo '============================
+      SSH Key Installer
+	 V1.0 Alpha
+	Author:Kirito
+============================'
+cd ~
+mkdir .ssh
+cd .ssh
+curl https://github.com/$1.keys > authorized_keys
+chmod 700 authorized_keys
+cd ../
+chmod 600 .ssh
+cd /etc/ssh/
+
+sed -i "/PasswordAuthentication no/c PasswordAuthentication no" sshd_config
+sed -i "/RSAAuthentication no/c RSAAuthentication yes" sshd_config
+sed -i "/PubkeyAuthentication no/c PubkeyAuthentication yes" sshd_config
+sed -i "/PasswordAuthentication yes/c PasswordAuthentication no" sshd_config
+sed -i "/RSAAuthentication yes/c RSAAuthentication yes" sshd_config
+sed -i "/PubkeyAuthentication yes/c PubkeyAuthentication yes" sshd_config
+sed -i "/PermitRootLogin/d" /etc/ssh/sshd_config
+sed -i '$a PermitRootLogin yes' /etc/ssh/sshd_config
+
+service sshd restart
+service ssh restart
+systemctl restart sshd
+systemctl restart ssh
+cd ~
+rm -rf key.sh
