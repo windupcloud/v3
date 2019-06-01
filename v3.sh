f229fea5a46045752396f521a4f294191ee2fc5e
Super-box <37006424+Super-box@users.noreply.github.com>
2019-06-01 19:32:11 +0800

    更改获取IP地址

--- v3.sh
+++ v3.sh
@@ -4,7 +4,7 @@
 [ $(id -u) != "0" ] && { echo "请切换至root账户执行此脚本."; exit 1; }
 
 #全局变量
-server_ip=`curl -s https://app.52ll.win/ip/api.php`
+server_ip=`curl -s https://ipv4.vircloud.net`
 separate_lines="####################################################################"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
