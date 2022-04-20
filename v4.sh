374f0a3b41ce97e9992ac0cc80472d3d499814ba
GGboy <472245834@qq.com>
2022-04-20 21:42:33 +0800

    curl IP地址

--- v4.sh
+++ v4.sh
@@ -4,7 +4,6 @@
 [ $(id -u) != "0" ] && { echo "请切换至root账户执行此脚本."; exit 1; }
 
 #全局变量
-server_ip=`curl -s http://ifconfig.co`
 separate_lines="####################################################################"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
@@ -153,7 +152,7 @@ ddns_install(){
 }
 
 get_server_ip_info(){
-    echo "ip"
+    server_ip_info=$(curl -s ifconfig.co)
 }
 
 gost_install(){
